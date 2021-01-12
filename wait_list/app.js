b4047b80634354de368a5a6be6a88a2448c1c12f
leec36 <leec36@montclair.edu>
2021-01-12 15:06:19 +0000

    server connection, fix DB connection, some changes in home.ejs and  add style sheet

--- wait_list/app.js
+++ wait_list/app.js
@@ -1,6 +1,21 @@
 var express = require('express');
+var mysql = require('mysql');
+
 
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var connection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	database : 'wait_list'
+});
+
+connection.query('SELECT * FROM users', function(err, result) {
+	if(err) throw err;
+	console.log(result);
+});
 
 app.set("view engine", "ejs");
 
